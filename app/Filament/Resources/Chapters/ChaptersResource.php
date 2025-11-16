a5cbc95515a4d771cb5db3a7eb5a4397101d5834
Rara <37941644+Ragy2001@users.noreply.github.com>
2025-11-16 01:30:08 +0100

    création chapitres dossiers attempts

---
+++ app/Filament/Resources/Chapters/ChaptersResource.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Filament\Resources\Chapters;
+
+use App\Filament\Resources\Chapters\Pages\CreateChapters;
+use App\Filament\Resources\Chapters\Pages\EditChapters;
+use App\Filament\Resources\Chapters\Pages\ListChapters;
+use App\Filament\Resources\Chapters\Schemas\ChaptersForm;
+use App\Filament\Resources\Chapters\Tables\ChaptersTable;
+use App\Models\Chapters;
+use BackedEnum;
+use Filament\Resources\Resource;
+use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ChaptersResource extends Resource
+{
+    protected static ?string $model = Chapters::class;
+
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+
+    protected static ?string $recordTitleAttribute = 'Chapter';
+
+    public static function form(Schema $schema): Schema
+    {
+        return ChaptersForm::configure($schema);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return ChaptersTable::configure($table);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => ListChapters::route('/'),
+            'create' => CreateChapters::route('/create'),
+            'edit' => EditChapters::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getRecordRouteBindingEloquentQuery(): Builder
+    {
+        return parent::getRecordRouteBindingEloquentQuery()
+            ->withoutGlobalScopes([
+                SoftDeletingScope::class,
+            ]);
+    }
+}
---
+++ app/Filament/Resources/Dossiers/Schemas/DossierForm.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Filament\Resources\Dossiers\Schemas;
+
+use Filament\Schemas\Schema;
+
+class DossierForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                //
+            ]);
+    }
+}
