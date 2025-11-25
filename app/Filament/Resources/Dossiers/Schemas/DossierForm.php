07a2a3a147078eaf363e137cdc899d9af8b7a3aa
Ragz <ragyedward2001@gmail.com>
2025-11-25 17:13:34 +0100

    reuse Q.form in Dossier repeater

--- app/Filament/Resources/Dossiers/Schemas/DossierForm.php
+++ app/Filament/Resources/Dossiers/Schemas/DossierForm.php
@@ -2,6 +2,10 @@
 
 namespace App\Filament\Resources\Dossiers\Schemas;
 
+use App\Filament\Resources\Questions\QuestionResource;
+use App\Filament\Resources\Questions\Schemas\QuestionForm;
+use Filament\Forms\Components\Repeater;
+use Filament\Schemas\Components\Form;
 use Filament\Schemas\Schema;
 
 class DossierForm
@@ -10,7 +14,9 @@ class DossierForm
     {
         return $schema
             ->components([
-                //
+                Repeater::make("Questions")
+                ->schema(QuestionForm::schemaArray())
+                ->columnSpanFull(),
             ]);
     }
 }
