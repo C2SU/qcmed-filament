197c225c7bb9fd4b398fa7ddc35cd70ae16f4211
Ragz <ragyedward2001@gmail.com>
2025-11-26 14:25:53 +0100

    hasmany questions relation to dosseir

--- app/Models/Dossier.php
+++ app/Models/Dossier.php
@@ -3,9 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Dossier extends Model
 {
     use SoftDeletes;
+
+    public function questions(): HasMany
+    {
+        return $this->hasMany(Question::class);
+    }
 }
