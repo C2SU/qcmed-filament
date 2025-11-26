7ea44205f821d3e324eb45d383c4f657b1bd068b
Ragz <ragyedward2001@gmail.com>
2025-11-26 19:03:07 +0100

    add title & description to Dossier form

--- app/Filament/Resources/Dossiers/Schemas/DossierForm.php
+++ app/Filament/Resources/Dossiers/Schemas/DossierForm.php
@@ -7,6 +7,8 @@ use App\Filament\Resources\Questions\Schemas\QuestionForm;
 use Barryvdh\Debugbar\Facades\Debugbar;
 use Closure;
 use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Components\Form;
 use Filament\Schemas\Components\Utilities\Get;
 use Filament\Schemas\Schema;
@@ -17,21 +19,34 @@ class DossierForm
     {
         return $schema
             ->components([
+                TextInput::make("title")
+                    ->columnSpanFull(),
+                
+                RichEditor::make("description")
+                    ->columnSpanFull()
+                    ->toolbarButtons([
+                                ['bold', 'italic', 'underline', 'strike', 'subscript', 'superscript', 'link'],
+                                ['h2', 'h3', 'alignStart', 'alignCenter', 'alignEnd'],
+                                ['bulletList', 'orderedList'],
+                                ['attachFiles'], // The `customBlocks` and `mergeTags` tools are also added here if those features are used.
+                                ['undo', 'redo'],
+                            ]),
+
                 Repeater::make("questions")
-                ->relationship()
-                ->schema(QuestionForm::schemaArray())
-                ->orderColumn("dossier_order")
-                ->reorderable()
-                ->reorderableWithButtons()
-                ->columnSpanFull()
-                ->collapsible()
-                ->defaultItems(5)
-                ->minItems(3)
-                ->maxItems(8)
-                ->itemLabel(function (Repeater $component, array $state) :string {
-                    $numeroQuestionActuelle = array_search(array_search($state, $component->getState()), array_keys($component->getState()))+1;
-                   return "Question ".strval($numeroQuestionActuelle);
-                })                
+                    ->relationship()
+                    ->schema(QuestionForm::schemaArray())
+                    ->orderColumn("dossier_order")
+                    ->reorderable()
+                    ->reorderableWithButtons()
+                    ->columnSpanFull()
+                    ->collapsible()
+                    ->defaultItems(5)
+                    ->minItems(3)
+                    ->maxItems(8)
+                    ->itemLabel(function (Repeater $component, array $state) :string {
+                        $numeroQuestionActuelle = array_search(array_search($state, $component->getState()), array_keys($component->getState()))+1;
+                    return "Question ".strval($numeroQuestionActuelle);
+                    })                
                 ,
             ]);
     }
