a5769e5c9399451ce717f9421b91722327f36264
Ragz <ragyedward2001@gmail.com>
2025-12-04 19:33:27 +0100

    sql change dossier "body" to longText

--- database/migrations/2025_11_15_185835_create_dossiers_table.php
+++ database/migrations/2025_11_15_185835_create_dossiers_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('dossiers', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->text('description')->nullable();
+            $table->longText('description')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
