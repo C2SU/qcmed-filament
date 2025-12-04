0db9ec596f5e0385c2ce255a271d80d81fc133f5
Ragz <ragyedward2001@gmail.com>
2025-12-04 19:33:08 +0100

    fix Question model BelongsToMany

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 
@@ -65,9 +66,9 @@ class Question extends Model
         return $this->belongsTo(Dossier::class)->withDefault();
     }
 
-    public function learningObjectives()
+    public function learningObjectives(): BelongsToMany
     {
-        return $this->belongsToMany(LearningObjective::class)->withDefault();
+        return $this->belongsToMany(LearningObjective::class);
     }
 
     // public function choices()
