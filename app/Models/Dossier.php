b5fd4434847c03b9c3a7f827ec0d6ac29fd811a0
Ragz <ragyedward2001@gmail.com>
2025-12-06 16:04:51 +0100

    add hasFactory trait to dossier model

--- app/Models/Dossier.php
+++ app/Models/Dossier.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -10,6 +11,7 @@ use Illuminate\Support\Facades\DB;
 class Dossier extends Model
 {
     use SoftDeletes;
+    use HasFactory;
 
     protected $fillable = [
         "title",
