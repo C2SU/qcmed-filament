9cda458ee72aab36f8b24cdff93dfd21d3c05c5b
Ragz <ragyedward2001@gmail.com>
2025-12-06 16:07:03 +0100

    rename description column to body in dossier table

--- database/migrations/2025_11_15_185835_create_dossiers_table.php
+++ database/migrations/2025_11_15_185835_create_dossiers_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('dossiers', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->longText('description')->nullable();
+            $table->longText('body');
             $table->timestamps();
             $table->softDeletes();
         });
