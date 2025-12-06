ff1c68f5997a70c6eddcde6a04f68d42fca7e1f1
Ragz <ragyedward2001@gmail.com>
2025-12-06 16:05:27 +0100

    add SoftDeletes and HasFactory traits to LO model

--- app/Models/LearningObjective.php
+++ app/Models/LearningObjective.php
@@ -2,11 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class LearningObjective extends Model
 {
+    use SoftDeletes;
+    use HasFactory;
+
     protected $fillable = [
         'rang',
         'rubrique',
