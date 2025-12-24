c93e392188a0cc2eaffc000f9820301ad410c368
Ragz <ragyedward2001@gmail.com>
2025-12-24 02:00:47 +0100

    Add ChapterRelationManager

---
+++ app/Filament/Resources/Matieres/RelationManagers/ChaptersRelationManager.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Filament\Resources\Matieres\RelationManagers;
+
+use Filament\Actions\AssociateAction;
+use Filament\Actions\AttachAction;
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\CreateAction;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\DetachAction;
+use Filament\Actions\DetachBulkAction;
+use Filament\Actions\DissociateAction;
+use Filament\Actions\DissociateBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Actions\ForceDeleteAction;
+use Filament\Actions\ForceDeleteBulkAction;
+use Filament\Actions\RestoreAction;
+use Filament\Actions\RestoreBulkAction;
+use Filament\Actions\ViewAction;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Schemas\Schema;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\TrashedFilter;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ChaptersRelationManager extends RelationManager
+{
+    protected static string $relationship = 'chapters';
+
+    public function form(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('numero')
+                    ->required()
+                    ->numeric(),
+                Textarea::make('description')
+                    ->required()
+                    ->columnSpanFull(),
+            ]);
+    }
+
+    public function infolist(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextEntry::make('numero')
+                    ->numeric(),
+                TextEntry::make('description'),  
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('numero')
+            ->columns([
+                TextColumn::make('numero')
+                    ->numeric()
+                    ->sortable()
+                    ->weight("bold"),
+                TextColumn::make('description')
+                    ->searchable()
+                    ->limit(100)
+                    ->wrap()
+                    ->grow(),
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('deleted_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                TrashedFilter::make(),
+            ])
+            ->headerActions([
+                CreateAction::make(),
+                AttachAction::make()
+                    ->preloadRecordSelect()
+                    ->recordSelectSearchColumns(["description","numero"])
+                    ->multiple(),
+            ])
+            ->recordActions([
+                // ViewAction::make(),
+                EditAction::make(),
+                DetachAction::make(),
+                DeleteAction::make(),
+                ForceDeleteAction::make(),
+                RestoreAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DetachBulkAction::make(),
+                    DeleteBulkAction::make(),
+                    ForceDeleteBulkAction::make(),
+                    RestoreBulkAction::make(),
+                ]),
+            ])
+            ->modifyQueryUsing(fn (Builder $query) => $query
+                ->withoutGlobalScopes([
+                    SoftDeletingScope::class,
+                ]));
+    }
+}
